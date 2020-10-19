9032e6d1e1e814f09487155baa4a008ff83e74ec
Unweptpit <Unweptpit@users.noreply.github.com>
2020-10-19 22:19:08 +0200

    Xraynotifier Fix

--- src/me/Unweptpit/Features/XrayNotifier.java
+++ src/me/Unweptpit/Features/XrayNotifier.java
@@ -2,6 +2,7 @@ package me.Unweptpit.Features;
 
 
 import java.util.HashMap;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 
@@ -12,7 +13,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-
 import me.Unweptpit.Picnicstaff.PicnicStaff;
 import net.md_5.bungee.api.ChatColor;
 
@@ -26,38 +26,49 @@ public class XrayNotifier implements Listener {
 	}
 	
 	
-	public HashMap<String, Integer> map = new HashMap<>();
 	
+	
+	public HashMap<UUID, HashMap<String, Integer>> map = new HashMap<>(); 
 	
 	@EventHandler()
 	public void onBlockBreak(BlockBreakEvent e) { // Check the block mined, if there are no blocks of the same type in the surrounding area send a message saying how many blocks the player mined.
 		Player player = (Player) e.getPlayer();
 		String block = e.getBlock().getType().toString();
-		
+		UUID uuid = player.getUniqueId();
 		
 		if(!(player.hasPermission(main.getConfig().getString("permissions.permissionXray")))) { //Check if player needs to be checked for xray
 			for (String j : main.getConfig().getStringList("Blocks")) {  //Loop through the blocks in the config
-			if (j.equalsIgnoreCase(block)) {  // If mined block matches a block in config
-					if (map.get(block) == null) {
-						map.put(block, 1);
-					}
-					else {
-						int count = map.get(block);
-						map.put(block, (count + 1));
+				if (j.equalsIgnoreCase(block)) {  // If mined block matches a block in config
+					if (map.containsKey(uuid) == false) {
+						HashMap<String, Integer> map2 = new HashMap<>();
+						map.put(uuid, map2);
+
 					}
 					
+					if (map.get(uuid).get(block) == null) {
+						map.get(uuid).put(block, 1);
+					}
+					else {
+						int count = map.get(uuid).get(block);
+						map.get(uuid).put(block, (count + 1));
+					}
+				
 					String name = main.getConfig().getString("Names." + block);
 					Location loc = e.getBlock().getLocation();
 					World world = loc.getWorld();
 					
 					if (LoopBlocks(e.getBlock(), loc, world)) {
 						for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-					    	if (onlinePlayers.hasPermission(main.getConfig().getString("permissions.permissionStaff"))) {
-					    			int i = map.get(block);
-					    			onlinePlayers.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4Xray&8] &f" + player.getDisplayName() + "&c has mined &f" + i + " " + name));
-						    		map.put(block, 0);
-					        }
-					    }
+							if (onlinePlayers.hasPermission(main.getConfig().getString("permissions.permissionStaff"))) {
+								int i = map.get(uuid).get(block);
+								onlinePlayers.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4Xray&8] &f" + player.getDisplayName() + "&c has mined &f" + i + " " + name));
+								Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4Xray&8] &f" + player.getDisplayName() + "&c has mined &f" + i + " " + name));
+								map.get(uuid).put(block, 0);
+							}
+						}
+						int i = map.get(uuid).get(block);
+						Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&4Xray&8] &f" + player.getDisplayName() + "&c has mined &f" + i + " " + name));
+						map.get(uuid).put(block, 0);
 					}
 				}
 			}   
